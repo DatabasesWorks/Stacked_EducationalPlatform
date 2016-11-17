c3e05f0d58e166383b38ce42f1df70caad51f6e7
Tyler Gardner <gard.tp@gmail.com>
2016-11-16 19:27:08 -0700

    connect example

--- view.cpp
+++ view.cpp
@@ -20,10 +20,15 @@ View::View(QWidget *parent) :
     //Selects 2 and sets stretch factor to (90 is relative to other columns)
     grid->setColumnStretch(2, 90);
     //Adds widget (widget, row, colum, rowspan, colspan, alignment
-    grid->addWidget(new QPushButton("BUTTON1!"), 1,1,1,1, Qt::AlignCenter);
-    grid->addWidget(new QPushButton("BUTTON2!"), 1,2,1,1, Qt::AlignCenter);
+    QPushButton* button1 = new QPushButton("BUTTON1!");
+    QPushButton* button2 = new QPushButton("BUTTON2!");
+    QPushButton* button3 = new QPushButton("BUTTON3!");
+    grid->addWidget(button1, 1,1,1,1, Qt::AlignCenter);
+    grid->addWidget(button2, 1,2,1,1, Qt::AlignCenter);
+    grid->addWidget(button3, 1,3,1,1, Qt::AlignCenter);
 
-    grid->addWidget(new QPushButton("BUTTON3!"), 1,3,1,1, Qt::AlignCenter);
+    //Example of how to create signal/slot connecton for new buttons
+    connect(button1, SIGNAL(clicked()), this, SLOT(on_pushButton_clicked()));
 }
 
 View::~View()
