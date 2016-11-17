18cdca953d9d3cc8b04c0e4c576db61f9f251b04
Tyler Gardner <gard.tp@gmail.com>
2016-11-16 18:27:24 -0700

    Dynamic resizing example

--- view.cpp
+++ view.cpp
@@ -1,11 +1,27 @@
 #include "view.h"
 #include "ui_view.h"
+#include "QGraphicsView"
+#include "QGridLayout"
+#include "QPushButton"
 
 View::View(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::View)
 {
     ui->setupUi(this);
+    //Set gridlayout to centralWidget (optional -- also can do ui...I think)
+    QGridLayout* grid = new QGridLayout(ui->centralWidget);
+    //Selects 1 and sets stretch factor to 10 (10 is relative to other columns)
+    grid->setColumnStretch(1,10);
+    //Selects 2 and sets stretch factor to (90 is relative to other columns)
+    grid->setColumnStretch(2, 90);
+    //Adds widget (widget, row, colum, rowspan, colspan, alignment
+    grid->addWidget(new QPushButton("BUTTON1!"), 1,1,1,1, Qt::AlignCenter);
+    grid->addWidget(new QPushButton("BUTTON2!"), 1,2,1,1, Qt::AlignCenter);
+
+
+
+
 }
 
 View::~View()
