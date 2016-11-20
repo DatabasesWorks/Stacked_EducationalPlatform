ffee7459807020f5163d28078e6425ae6009e3ba
Tor <caladanblitx13@gmail.com>
2016-11-20 05:23:53 -0700

    added notes on db

--- Server/db.cpp
+++ Server/db.cpp
@@ -1,5 +1,9 @@
 #include "db.h"
 
+// A note on this class, don't use any global variables since this class will be accessed by the multi-threaded server.
+// Since sql is safe for multithreading just build queries and return the results. The way that the sockets are set up
+// it should be able to handle multi-threaded processes.
+
 DB::DB()
 {
 
