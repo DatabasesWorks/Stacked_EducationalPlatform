685e689e55e7f5106571db6979161efaf719c8e7
Tor <caladanblitx13@gmail.com>
2016-11-19 23:13:44 -0700

    adding files

---
+++ Client/message.h
@@ -0,0 +1,22 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+#include <SFML/Network/Packet.hpp>
+#include <SFML/System/String.hpp>
+#include <string>
+
+class Message{
+
+public:
+    sf::String command;
+    sf::String payload;
+    sf::Uint16 returnport;
+    Message(std::string cmd, std::string pl, unsigned short);
+    Message();
+    ~Message();
+    //small utility class to clean up code.
+    friend sf::Packet& operator <<(sf::Packet& packet, Message& msg);
+    friend sf::Packet& operator >>(sf::Packet& packet, Message& msg);
+};
+
+
+#endif
