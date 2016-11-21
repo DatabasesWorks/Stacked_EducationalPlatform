6a7b4cb81e5897d156eb8e2e8baad9fdd82edf0a
Zak <zakpeters13@gmail.com>
2016-11-20 22:24:31 -0700

    small syntax

--- Server/db.cpp
+++ Server/db.cpp
@@ -88,7 +88,7 @@ std::string DB::regUser(MYSQL* connection, std::string payload){
 
     //if teach and class present error
     //if student and class not present error
-         if((mysql_num_rows(result) != 0 && split.at(3)="1") || (mysql_num_rows(result) == 0 && split.at(3)=="0")){
+         if((mysql_num_rows(result) != 0 && split.at(3)=="1") || (mysql_num_rows(result) == 0 && split.at(3)=="0")){
              return "CLASSERROR";
          }
 
