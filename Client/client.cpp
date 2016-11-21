3c3580748b7a68194887e8bfb703118f1d17a0c6
Tyler Gardner <gard.tp@gmail.com>
2016-11-20 20:53:42 -0700

    graphics object ttest demo works -- must set image location

--- Client/client.cpp
+++ Client/client.cpp
@@ -96,7 +96,7 @@ void Client::on_pushButton_2_clicked()
 
 void Client::on_pushButton_3_clicked()
 {
-    setCentralWidget(new GraphicsObjectTest(this, QPoint(20, 20), QSize(360, 360), 0));
+    setCentralWidget(new GraphicsObjectTest());
 }
 
 void Client::on_pushButton_4_clicked()
--- Client/graphicsobjecttest.h
+++ Client/graphicsobjecttest.h
@@ -2,19 +2,19 @@
 #define GRAPHICSOBJECTTEST_H
 #include <graphicsobject.h>
 #include <SFML/Graphics/Drawable.hpp>
+#include <iostream>
 
 class GraphicsObjectTest : public GraphicsObject
 {
 public :
 
-    GraphicsObjectTest(QWidget* Parent, const QPoint& Position, const QSize& Size, unsigned int FrameTime);
-
-    ~GraphicsObjectTest();
+    explicit GraphicsObjectTest(QWidget *Parent = 0);
+    void onInit();
+    void onUpdate();
 
 private:
-//    sf::Image  myImage;
-    sf::Texture myImage;
-    sf::Sprite mySprite;
+    sf::Texture tex;
+    sf::Sprite spr;
     void OnInit();
     void OnUpdate();
     int rot=1;
