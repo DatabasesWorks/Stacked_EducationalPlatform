fc66634cf908ab05b8cbfcbd72f54459322d63db
Christopher Hoffmann <hoffdamann@gmail.com>
2016-12-04 14:02:01 -0700

    Changed gravity and magnitude of move force upon deletion in array

--- Client/arraypuzzle.cpp
+++ Client/arraypuzzle.cpp
@@ -1,7 +1,7 @@
 #include "arraypuzzle.h"
 ArrayPuzzle::ArrayPuzzle() : Puzzle() {
 
-    b2Vec2 grav(0, 0.1);
+    b2Vec2 grav(0, 0.98);
     thisWorld->SetGravity(grav);
 
     SpriteDefinition floordef(100, 200, b2_staticBody, "testbox");
@@ -11,7 +11,7 @@ ArrayPuzzle::ArrayPuzzle() : Puzzle() {
     inactive_components.push_back(floor);
 
     for(int i = 0; i < 5; i++){
-        this->addComponent("array_"+i, 4, 100, 25, 100+(i*75), -200, b2_dynamicBody);
+        this->addComponent("array_"+i, 4, 100, 25, 100+(i*75), -25, b2_dynamicBody);
     }
     activeIndex = 0;
 }
@@ -52,10 +52,10 @@ void ArrayPuzzle::deleteAtIndexAction(){
         activeIndex--;
     }
     if(components.size() > 1){
-        for(int i = activeIndex; i < components.size(); i++){
+        for(int i = activeIndex; i < ((int)components.size()); i++){
             sprite2dObject * obj = (components[i]);
 
-            obj->moveBody(sprite2dObject::left, 15);
+            obj->moveBody(sprite2dObject::left, 300);
         }
     }
 }
@@ -68,7 +68,7 @@ void ArrayPuzzle::addAtIndexAction(){
      this->thisWorld->DestroyBody(bod);
      //might want to restrict the deletion if size = 1
      components.erase(components.begin() + activeIndex);
-     addComponent("array_"+activeIndex, 4, 100, 25, x, -200, b2_dynamicBody);
+     addComponent("array_"+activeIndex, 4, 100, 25, x, -25, b2_dynamicBody);
 }
 
 void ArrayPuzzle::sortArrayAction(){
