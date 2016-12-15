8da9dddf1ed5bb47e07437e8545a59e23977ce4d
Zak <zakpeters13@gmail.com>
2016-12-14 23:57:28 -0700

    fixed registration bug

--- Client/studreg.cpp
+++ Client/studreg.cpp
@@ -31,43 +31,42 @@ void StudReg::on_cancelButton_clicked() {
 }
 
 void StudReg::on_regButton_clicked() {
-    if (!Validate(ui->userEntry->text())) {
-        QMessageBox messageBox;
-        messageBox.critical(0,"ERROR", "Usernames must be 4+ alphanumeric characters");
-        messageBox.setFixedSize(500,200);
-        ui->userEntry->setText("");
-    }
-    if (!Validate(ui->passEntry->text())) {
-        QMessageBox messageBox;
-        messageBox.critical(0,"ERROR", "Passwords must be 4+ alphanumeric characters");
-        messageBox.setFixedSize(500,200);
-        ui->passEntry->setText("");
-    }
+    QRegExp re("[A-Za-z0-9]{4,16}");
 
-    Client *par = client;
-    QString data = ui->userEntry->text() + "," + ui->passEntry->text() + "," + ui->classEntry->text() + ",0," + ui->dobEntry->text();
-    int x = par->sendReg(data);
+    if (!re.exactMatch(ui->userEntry->text())) {
+        QMessageBox messageBox;
+        messageBox.critical(0, "ERROR", "Usernames must be 4+ alphanumeric characters");
+        messageBox.setFixedSize(500, 200);
+        ui->userEntry->setText("");
+    } else if (!re.exactMatch(ui->passEntry->text())) {
+        QMessageBox messageBox;
+        messageBox.critical(0, "ERROR", "Passwords must be 4+ alphanumeric characters");
+        messageBox.setFixedSize(500, 200);
+        ui->passEntry->setText("");
+    } else {
+        Client *par = client;
+        QString data = ui->userEntry->text() + "," + ui->passEntry->text() + "," + ui->classEntry->text() + ",0," + ui->dobEntry->text();
+        int x = par->sendReg(data);
+
         //username taken
-        if(x==1){
+        if (x == 1) {
             QString error = "Username: " + ui->userEntry->text() + " is already taken";
             QMessageBox messageBox;
-            messageBox.critical(0,"ERROR", error);
-            messageBox.setFixedSize(500,200);
+            messageBox.critical(0, "ERROR", error);
+            messageBox.setFixedSize(500, 200);
             ui->userEntry->setText("");
         }
         //class code taken
-        else if(x==2){
+        else if (x == 2) {
             QString error = "Class Code: " + ui->classEntry->text() + " is invalid";
             QMessageBox messageBox;
-            messageBox.critical(0,"ERROR", error);
-            messageBox.setFixedSize(500,200);
+            messageBox.critical(0, "ERROR", error);
+            messageBox.setFixedSize(500, 200);
             ui->classEntry->setText("");
         }
+    }
 }
 
 //alphanumeric 4-16
 bool StudReg::Validate(QString s) {
-    QRegExp re("[A-Za-z0-9]{4,16}");
-
-    return re.exactMatch(s);
 }
